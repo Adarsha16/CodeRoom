a0e5aaa4bbbbac6ca084982f2ffa4e27a522c4b5
sauhardhakafle <sauhardhakafle0@gmail.com>
2024-05-20 20:38:59 +0545

    changes

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,15 @@
 import { compiler } from "./compiler.js";
 import { Github } from "./github.js";
+<<<<<<< HEAD
 
 
 export { compiler, Github }
+=======
+import { login } from "./login.js";
+import { register } from "./register.js";
+import { createJWT, bcryptHash, emailValidation,verifyBcryptHash } from "./validator.js";
+import { room } from "./room.js";
+
+
+export { compiler, Github, login, register, emailValidation, createJWT, room, bcryptHash,verifyBcryptHash }
+>>>>>>> c318300 (Implementation Password Hashing with bcryptjs)
