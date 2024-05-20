c0f516c659450c67cccd688900a1134e44f02051
Ostrich-egg <sauhardhakafle12@gmail.com>
2024-05-20 20:48:52 +0545

    correction

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,25 +1,10 @@
-import { compiler } from "./compiler.js";
 import { Github } from "./github.js";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-export { compiler, Github }
-=======
+import { compiler } from "./compiler.js";
 import { login } from "./login.js";
 import { register } from "./register.js";
-import { createJWT, bcryptHash, emailValidation,verifyBcryptHash } from "./validator.js";
+import { createJWT, bcryptHash, emailValidation, verifyBcryptHash } from "./validator.js";
 import { room } from "./room.js";
 
 
-export { compiler, Github, login, register, emailValidation, createJWT, room, bcryptHash,verifyBcryptHash }
->>>>>>> c318300 (Implementation Password Hashing with bcryptjs)
-=======
-import { login } from "./login.js";
-import { register } from "./register.js";
-import { createJWT, bcryptHash, emailValidation,verifyBcryptHash } from "./validator.js";
-import { room } from "./room.js";
+export { compiler, Github, login, register, emailValidation, createJWT, room, bcryptHash, verifyBcryptHash }
 
-
-export { compiler, Github, login, register, emailValidation, createJWT, room, bcryptHash,verifyBcryptHash }
->>>>>>> upstream/main
