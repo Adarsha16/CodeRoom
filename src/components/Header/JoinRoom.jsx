fd52ef96876ca4c3bb0af80eeb2177bf1028eb9d
Adarsha <adarshapant350@gmail.com>
2024-06-01 19:26:59 +0545

    Added input fields for room components

--- src/components/Header/JoinRoom.jsx
+++ src/components/Header/JoinRoom.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import Button from "../Button";
 
 function JoinRoom() {
+    const [displayButton, setDisplayButton] = useState(true);
+
+    const handleClick = () => {
+        setDisplayButton(!displayButton);
+    }
     return (
         <>
-            <Button
+            {displayButton ? (<Button
                 type={'button'}
-                custom_class={'bg-white text-black rounded-md px-4 py-1'}
+                custom_class={'bg-white text-black rounded-md px-4 py-1 '}
                 buttonLabel={'Join Room'}
-            />
-        </>
-    );
+                handleClick={handleClick}
+            />) : (<div className="m-1 gap-1">
+                <input type="text" placeholder="Enter room code" className="w-40 h-8 rounded-md gap-1 bg-brown focus:outline-none" />
+                <Button
+                    type={'button'}
+                    custom_class={'bg-white text-black rounded-md px-4 py-1'}
+                    buttonLabel={'Join'}
+                />
+            </div>)}
+        </>);
 };
 
 export default JoinRoom;
