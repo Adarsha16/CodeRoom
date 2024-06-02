b771bf46b2b374e98bce0cdca4d92bda1cc64ad5
Ostrich_egg <Sauhardhakafle12@gmail.com>
2024-06-02 22:13:01 +0545

    Update on Room UI

--- src/components/Home-components/Chat.jsx
+++ src/components/Home-components/Chat.jsx
@@ -32,9 +32,11 @@ function Chat() {
 
     return (
         <>
-            <div className='absolute bottom-0 h-96 rounded-md border border-brown w-full'>
+            <div className='absolute bottom-0 h-4/6 rounded-md border border-brown w-full'>
                 {/*for general display elements*/}
-                < div className="px-8 pt-1  text-primary w-full " >
+                < div className="px-8 pb-2  text-primary w-full border-b-[1px] border-brown rounded-xl" >
+
+                    {/* Cross Button */}
                     <Button
                         custom_class={'pl-56 text-brown'}
                         buttonLabel={
@@ -45,14 +47,10 @@ function Chat() {
                                 <path fill="currentColor" d="M7.293 8L3.146 3.854a.5.5 0 1 1 .708-.708L8 7.293l4.146-4.147a.5.5 0 0 1 .708.708L8.707 8l4.147 4.146a.5.5 0 0 1-.708.708L8 8.707l-4.146 4.147a.5.5 0 0 1-.708-.708z" />
                             </svg>
                         } />
+
                     <p className="text-3xl font-semibold">Room</p>
                     <p className="text-white">Coding Pratice #4111fop</p>
                     <p>Profile pics here</p>
-                    {/* <input placeholder="room" onChange={(e) => {
-                        setRoom(e.target.value);
-                    }} />
-                    <button onClick={joinRoom}>Room</button>
-                */}
 
 
 
@@ -60,7 +58,8 @@ function Chat() {
 
                 {/*for chat main body */}
 
-                <div id="message_container" className="h-44 overflow-scroll mt-1 px-8">
+                <div id="message_container" className="h-full overflow-scroll mt-3 px-3 text-slate-400 text-sm">
+
                     {messageReceived.length === 0 ? (
                         <p>No messages yet</p>
                     ) : (
@@ -68,26 +67,35 @@ function Chat() {
                             <p key={index}>Person: {msg}</p>
                         ))
                     )}
+
                 </div>
 
                 {/*for chat input*/}
-                <div className="absolute bottom-0 h-20">
-                    <form id="send_container" onSubmit={handleSubmit} name="message_form">
-                        <input type="text"
+                <div className="fixed w-full bottom-0 p-0 m-0 flex items-center">
+
+                    <form id="send_container" onSubmit={handleSubmit} name="message_form" className="flex justify-center items-center">
+
+                        <input
+                            type="text"
                             placeholder="Start a conversation"
-                            className=" rounded-md h-10 bg-brown"
+                            className="p-5 h-10 w-full bg-brown border-none outline-none"
                             id='message'
+
                             onChange={(e) => {
                                 e.preventDefault();
                                 setMessage(e.target.value);
                             }
+
                             } />
                         <Button
-                            custom_class={'bg-primary rounded-md h-10 w-20 ml-1.5'}
+                            custom_class={'bg-primary w-1/6 h-10'}
                             type={"Submit"}
-                            buttonLabel={'Send'}
+                            buttonLabel={'^'}
                             id={"send_button"}
                         />
+
+
+
                     </form>
                 </div>
             </div>
