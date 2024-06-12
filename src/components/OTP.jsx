30da14675ceb1138e6ee9460ab35325581bbffa2
Adarsha <adarshapant350@gmail.com>
2024-06-12 19:23:33 +0545

    UI tweaks

--- src/components/OTP.jsx
+++ src/components/OTP.jsx
@@ -18,8 +18,8 @@ export default function OTP() {
                     <div className='flex flex-col gap-1.5 mb-4 items-center text-white'>
 
                         <h1 className='fira-sans-bold text-5xl mb-4 '>Code Room</h1>
-                        <h5 className=' text-2xl '>Enter your OTP code!</h5>
-                        <p>Check your gmail for OTP code</p>
+                        <h5 className=' text-2xl '>Verify your email</h5>
+                        <p>Check your email for OTP code</p>
 
                     </div>
 
