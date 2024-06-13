9ebc4278ad772ae9639ff7276b425b653e70846e
ostrich-egg <Sauhardhakafle12@gmail.com>
2024-06-13 14:04:27 +0545

    Now otp page is not allowed after signup

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -13,7 +13,8 @@ const initialState = {
 
     loginStatus: false,
     userData: null,
-    token: null
+    token: null,
+    signupStatus: false
 
 
 };
@@ -37,7 +38,7 @@ const authSlice = createSlice({
             state.token = token;
             state.userData = data;
 
-            console.log("Logged in state : ", state.userData, state.token)
+            console.log("Logged in state  userdata, token, signupstatus: ", state.userData, state.token, state.signupStatus)
 
         },
 
@@ -50,11 +51,18 @@ const authSlice = createSlice({
             state.userData = null;
             state.token = null;
         }
+        ,
+
+        setSignup: (state, action) => {
+
+            console.log("Signup", action.payload)
+            state.signupStatus = action.payload;
+        }
 
     }
 });
 
 
 export default authSlice.reducer;
-export const { login, logout, token } = authSlice.actions;
+export const { login, logout, token, setSignup } = authSlice.actions;
 
