2df13106e57cc235b0d2613aa58d20b4df93d5ee
ostrich-egg <Sauhardhakafle12@gmail.com>
2024-06-13 18:40:25 +0545

    Rate limitter and comments

--- server/router/route.js
+++ server/router/route.js
@@ -3,18 +3,25 @@ const router = express.Router();
 
 import { compiler, getUser, Github, login, register, beforeRegisteration } from "../controller/index.js"
 import { otpVerification } from '../middlewares/otpVerifcation.js';
+import { rateLimit } from 'express-rate-limit';
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    limit: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: {
+        msg: 'To many requests from this IP. Please Try again later'
+    }
 
+})
 
 router.post("/github", Github);
 router.route("/code/:lang").post(compiler);
-router.route("/auth/login").post(login);
+router.route("/auth/login").post(limiter, login);
 
 // router.route("/auth/register").post(register);
-router.route("/auth/register").post(beforeRegisteration);
+router.route("/auth/register").post(limiter, beforeRegisteration);
 router.route("/auth/otp/registeration").post(otpVerification, register);
 
-router.route("/getuser").get(getUser);
-
+router.route("/getuser").get(limiter, getUser);
 
 export default router
